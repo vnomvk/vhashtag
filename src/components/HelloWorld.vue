6a94d94744a56382349e71cad5da8d30d99488e0
VinothKumarM <vnomvk@gmail.com>
2018-03-06 17:46:46 +0530

    Basic in vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,95 +1,40 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1>messgae : {{ msg }}</h1>
+    <p>computed message : {{rMsg}}</p>
+    <p>Watch message : {{wMsg}}</p>
+    <p>Method message : {{mMsg()}}</p>
+    <input type='text' v-model='msg' />
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: "Welcome to VnoMvk VueLab",
+      wMsg: "hi"
     };
+  },
+  methods: {
+    mMsg: function() {
+      return this.msg;
+    }
+  },
+  computed: {
+    rMsg: function() {
+      return this.msg
+        .split("")
+        .reverse()
+        .join("");
+    }
+  },
+  watch: {
+    msg: function() {
+      return (this.wMsg = "Changes Accurs" + this.msg);
+    }
   }
 };
 </script>
@@ -99,16 +44,5 @@ export default {
 h1,
 h2 {
   font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
